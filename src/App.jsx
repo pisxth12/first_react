053d38b8400f069c5b1124af398e6928bace77ba
pisxth12 <pxsxth1214@gmail.com>
2025-07-14 20:20:21 +0700

    Your message describing the changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from './web/Header'
 import Nav from './web/Nav'
+import { HashRouter, Routes, Route } from 'react-router-dom'
+
 import Main from './web/Main'
 import Footer from './web/footer'
 
